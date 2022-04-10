a519068f99948f7a39554e44ebe084cc774e3753
ChandlerNgo <chandlerngo00@gmail.com>
2022-04-10 01:47:50 -0500

    models update and created migration 0001

--- Code2CollegeBankingProject/BankingUI/models.py
+++ Code2CollegeBankingProject/BankingUI/models.py
@@ -3,17 +3,17 @@ from django.db import models
 
 # Create your models here.
 class User_Info(models.Model):
-    first_name = models.CharField()
-    last_name = models.CharField()
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
     birthday = models.DateField()
     email = models.EmailField()
-    
+
 class User_Money(models.Model):
     money = models.IntegerField()
 
 class User_Names(models.Model):
-    pin_number = models.IntegerField(max_length=4)
+    pin_number = models.IntegerField()
     is_admin = models.BooleanField(default="False")
-    username = models.CharField()
+    username = models.CharField(max_length=30)
     info = models.ForeignKey(User_Info,on_delete=models.CASCADE)
     money = models.ForeignKey(User_Money,on_delete=models.CASCADE)
