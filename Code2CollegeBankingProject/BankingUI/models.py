c7a5233e4d24f8d0ab426a02df0fe3db3efce8b3
ChandlerNgo <chandlerngo00@gmail.com>
2022-04-10 01:32:27 -0500

    created models/tables for SQL using Django models.py

--- Code2CollegeBankingProject/BankingUI/models.py
+++ Code2CollegeBankingProject/BankingUI/models.py
@@ -1,3 +1,19 @@
+from http.cookiejar import DefaultCookiePolicy
 from django.db import models
 
 # Create your models here.
+class User_Info(models.Model):
+    first_name = models.CharField()
+    last_name = models.CharField()
+    birthday = models.DateField()
+    email = models.EmailField()
+    
+class User_Money(models.Model):
+    money = models.IntegerField()
+
+class User_Names(models.Model):
+    pin_number = models.IntegerField(max_length=4)
+    is_admin = models.BooleanField(default="False")
+    username = models.CharField()
+    info = models.ForeignKey(User_Info,on_delete=models.CASCADE)
+    money = models.ForeignKey(User_Money,on_delete=models.CASCADE)
