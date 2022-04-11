a6d42d38957cf111132594f6ff42211653a04bc4
ChandlerNgo <chandlerngo00@gmail.com>
2022-04-11 17:29:27 -0500

    fixed html

--- Code2CollegeBankingProject/BankingUI/views.py
+++ Code2CollegeBankingProject/BankingUI/views.py
@@ -5,3 +5,9 @@ from django.template import loader
 def index(request):
   template = loader.get_template('index.html')
   return HttpResponse(template.render())
+
+def createaccount(request):
+    return render(request, 'createaccount.html')
+
+def forgotpassword(request):
+    return render(request, 'forgotpassword.html')
