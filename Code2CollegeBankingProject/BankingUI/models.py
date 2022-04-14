a6fa886fd92d591ddf0a2adcc6e6787733a4ed7e
ChandlerNgo <chandlerngo00@gmail.com>
2022-04-14 18:13:59 -0500

    models fix

--- Code2CollegeBankingProject/BankingUI/models.py
+++ Code2CollegeBankingProject/BankingUI/models.py
@@ -2,18 +2,22 @@ from http.cookiejar import DefaultCookiePolicy
 from django.db import models
 
 # Create your models here.
-class User_Info(models.Model):
+class Customer(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     birthday = models.DateField()
     email = models.EmailField()
 
-class User_Money(models.Model):
-    money = models.IntegerField()
-
-class User_Names(models.Model):
+class BankInfo(models.Model):
     pin_number = models.IntegerField()
     is_admin = models.BooleanField(default="False")
     username = models.CharField(max_length=30)
-    info = models.ForeignKey(User_Info,on_delete=models.CASCADE)
-    money = models.ForeignKey(User_Money,on_delete=models.CASCADE)
+    customer_info = models.ForeignKey(Customer,on_delete=models.CASCADE)
+
+class Transactions(models.Model):
+    date = models.IntegerField()
+    withdrawal_deposit = models.IntegerField()
+    amount = models.IntegerField()
+    account = models.ForeignKey(BankInfo.id, on_delete=models.CASCADE)
+    
+# ask will to tell me why you cant put all attributes in one table
