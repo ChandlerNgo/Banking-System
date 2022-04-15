7c70dadf2547ab47f9badcce3f6a4ecd4e5d3cf5
ChandlerNgo <chandlerngo00@gmail.com>
2022-04-15 01:17:15 -0500

    fixed model name and added csrf token that doesn't work

--- Code2CollegeBankingProject/BankingUI/models.py
+++ Code2CollegeBankingProject/BankingUI/models.py
@@ -10,14 +10,13 @@ class Customer(models.Model):
 
 class BankInfo(models.Model):
     pin_number = models.IntegerField()
-    is_admin = models.BooleanField(default="False")
     username = models.CharField(max_length=30)
     customer_info = models.ForeignKey(Customer,on_delete=models.CASCADE)
-
+    
 class Transactions(models.Model):
-    date = models.IntegerField()
-    withdrawal_deposit = models.IntegerField()
+    date = models.DateField()
+    is_withdrawal = models.BooleanField()
     amount = models.IntegerField()
-    account = models.ForeignKey(BankInfo.id, on_delete=models.CASCADE)
+    account = models.ForeignKey(BankInfo, on_delete=models.CASCADE)
     
 # ask will to tell me why you cant put all attributes in one table
