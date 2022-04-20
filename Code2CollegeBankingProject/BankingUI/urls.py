b7fc00521816a0b10e553ebc6c66120cfaff4645
Will Chang <will.chang@ni.com>
2022-04-20 01:59:26 -0500

    Move URLs from project level into app level

--- Code2CollegeBankingProject/BankingUI/urls.py
+++ Code2CollegeBankingProject/BankingUI/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
+    path("createaccount", views.createaccount, name="createaccount"),
+    path("forgotpassword", views.forgotpassword, name="forgotpassword"),
+    path("index", views.index, name="index"),
+    # path('BankingUI/changeaccountinfo',views.changeaccountinfo, name='changeaccountinfo'),
+    path("account", views.account, name="account"),
+    path("changemoney", views.changemoney, name="changemoney"),
 ]
--- Code2CollegeBankingProject/Code2CollegeBankingProject/urls.py
+++ Code2CollegeBankingProject/Code2CollegeBankingProject/urls.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
 from BankingUI import views
+
 urlpatterns = [
-    path('BankingUI/', include('BankingUI.urls')),
-    path('admin/', admin.site.urls),
-    path('BankingUI/createaccount',views.createaccount, name='createaccount'),
-    path('BankingUI/forgotpassword',views.forgotpassword, name='forgotpassword'),
-    path('BankingUI/index',views.index, name='index'),
-    path('BankingUI/changeaccountinfo',views.changeaccountinfo, name='changeaccountinfo'),
-    path('BankingUI/account',views.account, name='account'),
-    path('BankingUI/changemoney',views.changemoney, name='changemoney'),
+    path("BankingUI/", include("BankingUI.urls")),
+    path("admin/", admin.site.urls),
 ]
