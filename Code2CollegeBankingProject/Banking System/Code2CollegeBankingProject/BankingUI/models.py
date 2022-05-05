cb53249723346f80a4d0003445970899719277b3
ChandlerNgo <90358003+ChandlerNgo@users.noreply.github.com>
2022-05-05 17:59:18 -0500

    Add files via upload

---
+++ Code2CollegeBankingProject/Banking System/Code2CollegeBankingProject/BankingUI/models.py
@@ -0,0 +1,13 @@
+from http.cookiejar import DefaultCookiePolicy
+from django.db import models
+from django.contrib.auth.models import User
+
+User._meta.get_field("email")._unique = True
+User._meta.get_field("username")._unique = True
+
+
+class Transactions(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    transactiontype = models.CharField(max_length=8)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    account = models.ForeignKey(User, on_delete=models.CASCADE)
