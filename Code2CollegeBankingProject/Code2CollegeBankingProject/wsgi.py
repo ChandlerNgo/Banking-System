b458bbd32fc6758b2fa845cb7884c0605db7975b
ChandlerNgo <chandlerngo00@gmail.com>
2022-07-26 22:50:03 -0500

    changes

--- Code2CollegeBankingProject/Code2CollegeBankingProject/wsgi.py
+++ Code2CollegeBankingProject/Code2CollegeBankingProject/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Code2CollegeBankingProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Code2CollegeBankingProject.Code2CollegeBankingProject.settings')
 
 application = get_wsgi_application()
